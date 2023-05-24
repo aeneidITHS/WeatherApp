cca88bfbf354fa38564cd3a9443360e0447faa16
aeneidITHS <112550107+aeneidITHS@users.noreply.github.com>
2023-05-24 14:26:04 +0200

    Added a bunch of stuff to met

--- src/main/java/com/example/weatherapp/Bussiness/Entities/SHMIEntity.java
+++ src/main/java/com/example/weatherapp/Bussiness/Entities/SHMIEntity.java
@@ -1,9 +1,12 @@
 package com.example.weatherapp.Bussiness.Entities;
 
+import com.example.weatherapp.Bussiness.WeatherSMHI.Parameter;
 import com.example.weatherapp.Clients.SMHIClient;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Component
 public class SHMIEntity extends WeatherWebsiteTemplate{
     SMHIClient smhiClient;
@@ -11,9 +14,46 @@ public class SHMIEntity extends WeatherWebsiteTemplate{
         smhiClient = new SMHIClient();
         dataSource = "SMHI";
     }
+    public void updateInfo(){
+        temperature=getTemperatureByTime(hour);
+        humidity = getHumidityByTime(hour);
+        currentTime = getTimeBySMHI(hour);
+    }
+    public LocalDateTime getTimeBySMHI(int hour){
+        return LocalDateTime.parse(smhiClient.weatherServiceSHMI.getTimeSeries().get(checkIfTimeIsCorrect()+hour).getValidTime(),DateTimeFormatter.ISO_DATE_TIME);
+    }
+    public Integer getTemperatureByTime(int hour){
+        int temperature = 0;
+        for (Parameter p: smhiClient.weatherServiceSHMI.getTimeSeries().get(checkIfTimeIsCorrect()+hour).getParameters()){
+            if (p.getName().equals("t")){
+                temperature=p.getValues().get(0);
+            }
+        }
+        return temperature;
+    }
+    public Integer getHumidityByTime(int hour){
+        int humidity = 0;
+        for (Parameter p: smhiClient.weatherServiceSHMI.getTimeSeries().get(checkIfTimeIsCorrect()+hour).getParameters()) {
+            if (p.getName().equals("h")){
+                humidity = p.getValues().get(0);
+            }
+        }
+        return humidity;
+    }
 
-public boolean checkIfTimeIsCorrect(){
-
+public Integer checkIfTimeIsCorrect(){
+    boolean isCorrect = true;
+    int i = 0;
+    LocalDateTime localDateTime = LocalDateTime.parse(smhiClient.weatherServiceSHMI.getTimeSeries().get(0).getValidTime(), DateTimeFormatter.ISO_DATE_TIME);
+    while (isCorrect){
+        if (localDateTime.getHour() + i !=LocalDateTime.now().getHour()){
+            i++;
+        }
+        else {
+            isCorrect = false;
+        }
+    }
+    return i;
 }
 
 
---
+++ src/main/java/com/example/weatherapp/Bussiness/WeatherMET/Details__2.java
@@ -0,0 +1,47 @@
+
+package com.example.weatherapp.Bussiness.WeatherMET;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.annotation.processing.Generated;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "precipitation_amount"
+})
+@Generated("jsonschema2pojo")
+public class Details__2 {
+
+    @JsonProperty("precipitation_amount")
+    private Double precipitationAmount;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    @JsonProperty("precipitation_amount")
+    public Double getPrecipitationAmount() {
+        return precipitationAmount;
+    }
+
+    @JsonProperty("precipitation_amount")
+    public void setPrecipitationAmount(Double precipitationAmount) {
+        this.precipitationAmount = precipitationAmount;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
